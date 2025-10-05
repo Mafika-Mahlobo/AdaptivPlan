98b8000d834afb0b7e9392da2f96ae6d9c776d9a
Mafika <rudolphmafika@gmail.com>
2025-10-05 12:52:38 +0200

    restrict slot options to owner

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Task
+from availability_slots.models import AvailabilitySlots
 
 class TaskSerializer(serializers.ModelSerializer):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        request = self.context.get("request") 
+        if request and hasattr(request, "user"):
+            self.fields["slot"].queryset = AvailabilitySlots.objects.filter(user=request.user)
 
     class Meta:
         model = Task
